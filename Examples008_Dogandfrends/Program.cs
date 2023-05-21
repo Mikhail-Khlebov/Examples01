6b6966a54d25327df4425236c308b73e208c278c
Mikhail <277572@mail.ru>
2023-05-21 10:59:51 +0500

    examples008_dogandfrends

---
+++ Examples008_Dogandfrends/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+double dist = 10000;
+double FirstFrSpeed = 1;
+double SecFrSpeed = 2;
+double DogSpeed = 5;
+int Friend = 2;
+double time;
+while (dist > 10)
+{
+    if (Friend == 1)
+    {
+        time = dist / (FirstFrSpeed + DogSpeed);
+        Friend = 2;
+    }
+    else
+    {
+        time = dist / (SecFrSpeed + DogSpeed);
+        Friend = 1;
+    }
+    dist = dist - (FirstFrSpeed + SecFrSpeed) * time;
+    Console.WriteLine ("между друзьями отсалось " +Math.Round(dist,2)+" м.");
+    count++;
+} 
+Console.WriteLine ("Собака пробежала между друзьями " + count + " раз");
