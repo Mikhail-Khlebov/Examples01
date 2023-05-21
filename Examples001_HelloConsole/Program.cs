734caf8188abee4ebfd117a1c39aba706607dd5a
Mikhail <277572@mail.ru>
2023-05-21 06:45:11 +0500

    Examples_003Sum

--- Examples001_HelloConsole/Program.cs
+++ Examples001_HelloConsole/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿Console.WriteLine("Введите Ваше любимое слово : ");
+// Сохраняем в строковой переменной введенное пользователем слово
+string favouriteWord = Console.ReadLine()!;
+Console.WriteLine("Сколько раз его напечатать? ");
+// Сохраняем в целочисленной переменной введенное число
+// (При неправильном вводе числа произойдет ошибка)
+int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+// Выводим на экран слово указанное количество раз
+for (int i = 0; i < numberOfTimes; i++)
+{
+Console.WriteLine(favouriteWord);
+}
+// Ожидаем нажатия клавиши ВВОД
+Console.ReadLine();
---
+++ Examples003_Sam/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
