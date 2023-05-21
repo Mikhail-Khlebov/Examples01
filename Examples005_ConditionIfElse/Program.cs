7e59acf08c0ac73969713857bd4c6a775b000a43
Mikhail <277572@mail.ru>
2023-05-21 07:29:03 +0500

    Condotionifesle

---
+++ Examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Ввидите имя пользоателя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же МАША");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);
+}
