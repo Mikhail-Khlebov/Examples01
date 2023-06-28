467dbf74c1ba93b2778a5216270d4ad8e49f0ca7
277572@mail.ru <277572@mail.ru>
2023-06-28 19:56:18 +0500

    plus

--- Examples011_Methods/Program.cs
+++ Examples011_Methods/Program.cs
@@ -109,12 +109,11 @@
 // Console.WriteLine();
 // newText = Replace(text, 'с', 'С');
 // Console.WriteLine(newText);
-// int[] arr = { 1, 5, 4, 5, 3, 2, 6, 7, 1, 1 };
 
+// int[] arr = { 1, 5, 4, 5, 3, 2, 6, 7, 1, 1 };
 // void PrintArray(int[] array)
 // {
 //     int count = array.Length;
-
 //     for (int i = 0; i < count; i++)
 //     {
 //         Console.Write($"{array[i]} ");
@@ -141,34 +140,34 @@
 // SelectionSort(arr);
 // PrintArray(arr);
 
-int[] arr = { 1, 5, 4, 5, 3, 2, 6, 7, 1, 1 };
+// int[] arr = { 1, 5, 4, 5, 3, 2, 6, 7, 1, 1 };
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
 
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int maxPosition = i;
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] > array[maxPosition]) maxPosition = j;
-        }
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int maxPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[maxPosition]) maxPosition = j;
+//         }
 
-        int temporary = array[i];
-        array[i] = array[maxPosition];
-        array[maxPosition] = temporary;
-    }
-}
-PrintArray(arr);
-SelectionSort(arr);
-PrintArray(arr);
+//         int temporary = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
